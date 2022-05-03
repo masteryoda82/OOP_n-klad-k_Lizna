20eff2a9dde08f9dd487292d0f5d4c31c4d1fab6
Petr Lizna <Lizna.Petr@stud.vassboskovice.cz>
2022-05-03 09:00:11 +0200

    Přidejte soubory projektu.

---
+++ OOP_nákladák_Lizna/Nakladak.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_nákladák_Lizna
+{
+    internal class Nakladak
+    {
+        public string Nazev = "";
+        private int palivo;
+        public int Palivo { get => palivo; }
+        public string Nosnost = "";
+        public  string Objem_nadrzre = "";
+        private int naklad;
+        public int Naklad { get => naklad; }
+        public string Spotreba_nadrze = "";
+        private int celkova_vzdalenost;
+        public int Celkova_vzdalenost { get => celkova_vzdalenost; }
+
+    }
+    public  Nakladak()
+    {
+
+    }
+    
+        
+    
+}
